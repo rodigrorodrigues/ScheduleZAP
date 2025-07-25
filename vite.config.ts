2c7959860eab7fc7f4405614af70cf588e854a08
Rodrigo Rodrigues <rodrigodevrodrigues@gmail.com>
2025-07-25 01:07:30 -0300

    Atualiza a configuração do Vite para alterar a porta do servidor de 3000 para 8988.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 8988,
     host: true,
     proxy: {
       "/api": {
